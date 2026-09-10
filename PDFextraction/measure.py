e8041da7083046d332f3f02b2ceb7ecc4bbddd8a
Teo Hui Qian <73115813+raven0205@users.noreply.github.com>
2026-09-10 18:25:53 +0000

    added project 02-development

--- PDFextraction/measure.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber
-
-filepath = "visa-merchant-data-standards-manual.pdf"
-
-with pdfplumber.open(filepath) as pdf:
-    page = pdf.pages[27]
-    for table in page.find_tables():
-        print(table.bbox)  # gives (x0, top, x1, bottom)
